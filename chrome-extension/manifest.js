19cc7361cdc11e5a1efa246764d91edc31ea9888
Elliot Tica <elliottica937@gmail.com>
2025-06-23 21:36:43 +0800

    feat: remove storage permissions in manifest.js

--- chrome-extension/manifest.js
+++ chrome-extension/manifest.js
@@ -17,7 +17,7 @@ const manifest = {
   version: packageJson.version,
   description: '__MSG_extensionDescription__',
   host_permissions: ['<all_urls>'],
-  permissions: ['storage', 'scripting', 'tabs', 'activeTab', 'debugger', 'nativeMessaging'],
+  permissions: ['scripting', 'tabs', 'activeTab', 'debugger', 'nativeMessaging'],
   background: {
     service_worker: 'background.iife.js',
     type: 'module',
